a7d86d3997b1c4aaa2ca15407dc2484d0f186079
dannybarrus <danny.barrus@gmail.com>
2017-10-17 16:34:18 -0600

    Minor bug fix

--- java/SinglyLinkedList/src/singlylinkedlist/SinglyLinkedList.java
+++ java/SinglyLinkedList/src/singlylinkedlist/SinglyLinkedList.java
@@ -26,13 +26,13 @@ public class SinglyLinkedList
         thisList.add("4");
         thisList.add("5");
         
-        System.out.println("Print: crunchifyList: \t\t" + thisList);
+        System.out.println("Print:  \t\t" + thisList);
         System.out.println(".size(): \t\t\t\t" + thisList.size());
         System.out.println(".get(3): \t\t\t\t" + thisList.get(3) + " (get element at index:3 - list starts from 0)");
         System.out.println(".remove(2): \t\t\t\t" + thisList.remove(2) + " (element removed)");
         System.out.println(".get(3): \t\t\t\t" + thisList.get(3) + " (get element at index:3 - list starts from 0)");
         System.out.println(".size(): \t\t\t\t" + thisList.size());
-        System.out.println("Print again: crunchifyList: \t" + thisList);
+        System.out.println("Print again: \t\t" + thisList);
     }
     
 }
